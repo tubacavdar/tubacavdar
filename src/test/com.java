25de327bc6b5fad4934399785a24333eb7f7c174
tubacavdar <tubaacavdarr@gmail.com>
2021-12-09 21:25:50 -0500

    003

--- src/test/com.java
+++ src/test/com.java
@@ -3,7 +3,9 @@ package test;
 public class com {
     public static void main(String[] args) {
         System.out.println("Merhaba");
-        System.out.println("Merhaba");  
+        System.out.println("Merhaba");
+        System.out.println("Merhaba"); 
+
 
     }
 }
